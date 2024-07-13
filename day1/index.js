8332204b831f2ee61cf45db1ec811d68aad9f243
Rahul Parida <rparida1052@gmail.com>
2024-07-13 12:21:36 +0530

    day1

---
+++ day1/index.js
@@ -0,0 +1,28 @@
+var x = 9;
+let y = "Hello world"
+const z = true;
+console.log(x)
+console.log("value of let variable y is: ", y)
+console.log("value of const variable z is: ", z)
+
+// task 4
+
+const a =5;
+const b = "Rahul"
+const c = true;
+const d = {
+    name:"Rahul",
+    age:"19"
+}
+
+const e = [1,2,3,4,5]
+console.log("type of a is: ", typeof a)
+console.log("type of b is: ", typeof b)
+console.log("type of c is: ", typeof c)
+console.log("type of d is: ", typeof d)
+console.log("type of e is: ", typeof e)//object
+
+//task 5
+const var1 = 5;
+var1 = 10;
+console.log(var1)//TypeError: Assignment to constant variable.
